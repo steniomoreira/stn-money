0a1380e824d8ae66957ccffe248894cf831feff1
steniomoreira <steniomoreiradev@gmail.com>
2025-03-01 00:45:25 -0300

    fix: maintain the correct aspect of the image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,14 @@ function Header() {
     <header className="h-[212px] bg-black p-4 pt-8">
       <div className="m-auto flex max-w-[1200px] items-center justify-between">
         <div className="flex items-center gap-2">
-          <Image src={"/logo.svg"} alt="STN Money" width={40} height={38} />
+          <div className="relative h-10 w-9">
+            <Image
+              src="/logo.svg"
+              alt="STN Money"
+              fill
+              className="object-contain"
+            />
+          </div>
           <h1 className="text-2xl font-bold">STN Money</h1>
         </div>
         <Button>Nova transação</Button>
